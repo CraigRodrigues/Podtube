ba40220f73c12f02cdf7bdd4bc9b37d430dc5f91
Craig Rodrigues <PagingCraig@gmail.com>
2017-03-11 00:00:04 -0800

    Add controller to Podtube directive

--- app/components/podtube.js
+++ app/components/podtube.js
@@ -1,11 +1,12 @@
 angular.module('app')
-.controller('AppCtrl', function() {
-
+.controller('PodtubeCtrl', function() {
+  this.videos = window.exampleVideoData;
+  console.log('search results videos', this.videos);
 })
 .directive('podtube', function() {
   return {
     scope: {},
-    controller: 'AppCtrl',
+    controller: 'PodtubeCtrl',
     controllerAs: 'ctrl',
     bindToController: true,
     templateUrl: 'app/templates/podtube.html'
