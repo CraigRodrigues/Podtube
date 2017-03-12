7138f4e04e68e9fbfa54027226828d4c4ea7fc4e
Craig Rodrigues <PagingCraig@gmail.com>
2017-03-12 14:02:56 -0700

    Grab audio element for manipulation

--- app/components/podcastPlayer.js
+++ app/components/podcastPlayer.js
@@ -1,6 +1,17 @@
 angular.module('app')
 .controller('PodcastPlayerCtrl', function() {
+  this.player = player = document.getElementById('audioPlayer');
 
+  this.podcast;
+  this.previous = () => {
+    console.log(player);
+  };
+  this.next;
+  this.skipBackwards = () => {
+    console.log()
+  };
+  this.skipForwards;
+  this.changeSpeed;
 })
 .directive('podcastPlayer', function() {
   return {
